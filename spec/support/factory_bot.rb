a9bd28c9593952d790a18be321443bc9afa02012
Max <maxwell.devoe@gmail.com>
2018-02-07 11:11:13 -0500

    user authentication

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -3,6 +3,9 @@ require 'factory_bot'
 FactoryBot.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
+    sequence(:user_name) { |n| "user#{n}" }
+    first_name 'Jon'
+    last_name 'Snow'
     password 'password'
     password_confirmation 'password'
   end
